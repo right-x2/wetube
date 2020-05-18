40842af1ed93d7a539f608873e6cae255463a14f
right-x2 <kjw970103@gmail.com>
2020-05-18 23:47:22 +0900

    commit

---
+++ routers/globalRouter.js
@@ -0,0 +1,18 @@
+import express from "express";
+import routes from "../routes";
+import { home, search } from "../controllers/videoControllers";
+import {getLogin,postLogin, postJoin,getJoin, login, logout } from "../controllers/userControllers";
+
+const globalRouter = express.Router();
+
+globalRouter.get(routes.join,getJoin);
+globalRouter.post(routes.join,postJoin);
+
+globalRouter.get(routes.login,getLogin);
+globalRouter.post(routes.login,postLogin);
+
+globalRouter.get(routes.home,home);
+
+globalRouter.get(routes.logout,logout);
+globalRouter.get(routes.search,search);
+export default globalRouter;
